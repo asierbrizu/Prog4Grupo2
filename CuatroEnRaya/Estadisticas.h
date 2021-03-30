883ddb31eabbd047f5fd22e40717eb9a6dacf5f2
Asier Brizuela <asierbrizu@opendeusto.es>
2021-03-30 23:07:58 +0200

    Renombrar Ajustes

--- CuatroEnRaya/Estadisticas.h
+++ CuatroEnRaya/Estadisticas.h
@@ -6,9 +6,9 @@
  *      En este fichero se gestionan las estadísticas del jugador, se cuentan las partidas jugadas, ganadas y perdidas.
  */
 
-#ifndef SISTEMARANKING_H_
-#define SISTEMARANKING_H_
+#ifndef ESTADISTICAS_H_
+#define ESTADISTICAS_H_
 
 
 
-#endif /* SISTEMARANKING_H_ */
+#endif /* ESTADISTICAS_H_ */
